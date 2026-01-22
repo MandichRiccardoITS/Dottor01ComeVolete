41b720440e0b792df9559f28b478cc02cf39b300
RickyMandich <ricky.mandich@gmail.com>
2026-01-22 10:21:46 +0100

    aggiornamento 2026 01 22 10:21

--- ComeVolete/Program.cs
+++ ComeVolete/Program.cs
@@ -5,6 +5,7 @@ var name = Console.ReadLine();
 
 Console.WriteLine("Inserire una Lettera");
 var key = Console.ReadKey();
+Console.WriteLine();
 
 if(key.Key == ConsoleKey.Y)
 {
@@ -13,10 +14,25 @@ if(key.Key == ConsoleKey.Y)
 
 if (key.KeyChar == 'Y')
 {
-
+    Console.WriteLine("la y è maiuscola");
 }
 
 if (key.KeyChar == 'y')
 {
+    Console.WriteLine("la y è minuscola");
+}
 
+Console.WriteLine("inserire un numero");
+var numInput = Console.ReadLine();
+
+if (int.TryParse(numInput, out int num))
+{
+    if (num > 10)
+    {
+        Console.WriteLine("il numero è maggiore di 10");
+    }
+}
+else
+{
+    Console.WriteLine("il dato inserito non è un numero");
 }
